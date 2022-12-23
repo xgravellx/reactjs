8ca47577bfbd2dfc19b993b6048741d8a3211420
Hazal Yilmaz <88984994+xgravellx@users.noreply.github.com>
2022-12-23 17:16:58 +0300

    first commit

---
+++ reactProjects/emoji-search-app-ts/src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Styled } from './Header.styled'
+
+const Header = () => {
+  return (
+    <Styled>Header</Styled>
+  )
+}
+
+export default Header
---
+++ reactProjects/emoji-search-app-ts/src/components/Main/Main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Styled } from './Main.styled'
+
+const Main = () => {
+  return (
+    <Styled>Main</Styled>
+  )
+}
+
+export default Main
